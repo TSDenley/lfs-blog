d07fd7561d592317f8750ad21d83b2482f30a980
Tom Denley <tsdenley@gmail.com>
2018-09-09 14:02:30 +0100

    Posted on date format

--- resources/views/posts/article.blade.php
+++ resources/views/posts/article.blade.php
@@ -1,7 +1,7 @@
 <article class="post">
     <header class="post-header">
         <h1 class="post-title">{{ $post->title }}</h1>
-        <p class="post-meta">{{ $post->created_at }}</p>
+        <p class="post-meta">{{ $post->created_at->format('l, jS F Y \a\t H:i') }}</p>
     </header>
 
     <div class="post-body">
