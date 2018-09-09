dc0b581cbb1bcfaa6e961e852a7ba93b2313accb
Tom Denley <tsdenley@gmail.com>
2018-09-09 14:08:06 +0100

    Post title links to article; styling

--- resources/views/posts/article.blade.php
+++ resources/views/posts/article.blade.php
@@ -1,6 +1,9 @@
 <article class="post">
     <header class="post-header">
-        <h1 class="post-title">{{ $post->title }}</h1>
+        <h1 class="post-title">
+            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+        </h1>
+
         <p class="post-meta">{{ $post->created_at->format('l, jS F Y \a\t H:i') }}</p>
     </header>
 
