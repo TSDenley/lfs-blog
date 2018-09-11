f7912631d6d7efcf7798934d0bb475d27dfc2e5b
Tom Denley <tsdenley@gmail.com>
2018-09-11 18:37:47 +0100

    Register new users

---
+++ resources/views/registration/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layout')
+
+@section('content')
+    <h2>Register</h2>
+
+    @include('layouts.errors')
+
+    <form action="/register" method="POST" class="create-user-form">
+        {{ csrf_field() }}
+
+        <p>
+            <label for="name">Name</label>
+            <input id="name" name="name" type="text" required />
+        </p>
+
+        <p>
+            <label for="email">Email</label>
+            <input id="email" name="email" type="email" placeholder="me@example.com" required />
+        </p>
+
+        <p>
+            <label for="password">Password</label>
+            <input id="password" name="password" type="password" required />
+        </p>
+
+        <p>
+            <label for="password_confirmation">Confirm Password</label>
+            <input id="password_confirmation" name="password_confirmation" type="password" required />
+        </p>
+
+        <p><button type="submit" class="btn-default">Sign up</button></p>
+    </form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Route::get('/posts/{post}', 'PostController@show');
 Route::get('/posts', 'PostController@index');
 
 Route::post('/posts/{post}/comments', 'CommentsController@store');
+
+Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
+
+Route::get('/login', 'SessionsController@create');
