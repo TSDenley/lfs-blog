fa210d335c9ecc5fb81e59db03e775d4087ad26a
Tom Denley <tsdenley@gmail.com>
2018-09-18 15:04:09 +0100

    Feature test for welcome heading

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,10 +12,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+    public function testWelcome () {
+        $this->get('/')->assertSee('<h2>Welcome</h2>');
     }
 }
